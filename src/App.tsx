1b4bac2a66b4ed6287b6fbfb17ed9aa78a12c6ae
KhanhSohobb <khanh@sohobb.jp>
2025-04-08 18:00:09 +0900

    Add title, invalid move

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ const Chessboard = () => {
   const [hasSelectedStart, setHasSelectedStart] = useState(false)
   const [visitedSquares, setVisitedSquares] = useState<[number, number] []>([])
 
+  const knightMoves = [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [2, -1], [-2, -1]]
+
   const isValidMove = (from: [number, number], to: [number, number]) => {
     const dx = Math.abs(from[0] - to[0])
     const dy = Math.abs(from[1] - to[1])
@@ -16,14 +18,14 @@ const Chessboard = () => {
       setKnightPosition(nextPos);
       setHasSelectedStart(true);
       setVisitedSquares([nextPos]);
-    } else if (knightPosition && isValidMove(knightPosition, nextPos)) {
+    } else if (
+      knightPosition && isValidMove(knightPosition, nextPos) && !visitedSquares.some(([x, y]) => x === i && y === j)
+    ) {
       setKnightPosition(nextPos);
-      setVisitedSquares((prev) => {
-        const alreadyVisited = prev.some(([x, y]) => x === i && y === j)
-        return alreadyVisited ? prev : [...prev, nextPos]
-      })
+      setVisitedSquares((prev) => [...prev, nextPos]);
     }
-  }
+  };
+  
 
   const renderSquare = (i:number, j:number) => {
     const isKnight = knightPosition && knightPosition[0] === i && knightPosition[1] === j
@@ -59,14 +61,21 @@ const Chessboard = () => {
     return squares
   };
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold mb-4">Knight's Tour</h1>
-      <div className="grid border-4 border-black">
-        {renderBoard()}
-      </div>
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-100">
+      
+      <div>
+        <h1 className="text-3xl font-bold mb-4 text-center">Knight's Tour</h1>
+        <div className="grid grid-rows-8 border-4 border-black shadow-lg">
+          {renderBoard()}          </div>
+        </div>
+      <div>
+        <h2 className="text-3xl font-bold pl-20">Information</h2>
+        <p className="text-3xl font-bold pt-5 pl-20">Score: {visitedSquares.length}</p>
+        {/*<p className="text-4xl font-bold pt-5 pl-20">You dumb lost</p> */}
+      </div>  
     </div>
-  );
-
+    
+  )
 };
 
-export default Chessboard;  
+export default Chessboard;
